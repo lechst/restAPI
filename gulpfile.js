e6aa6e89fa830ad12c1bc744f7a2bb1d3a9ea106
Lech Stawikowski <lech.stawikowski@augmentersoft.com>
2019-09-14 20:44:25 +0200

    gulp and browserify full setup with mocha/chai tests

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,9 @@ var jshint = require('gulp-jshint');
 // Test Dependencies
 var mochaPhantomjs = require('gulp-mocha-phantomjs');
 
+
+// Lint
+
 gulp.task('lint-client', function() {
 	return gulp.src('./client/**/*.js')
 		.pipe(jshint())
@@ -28,6 +31,9 @@ gulp.task('lint-test', function() {
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
+
+
+// Browserify
 
 gulp.task('browserify-client', ['lint-client'], function() {
 	return gulp.src('client/index.js')
@@ -48,7 +54,50 @@ gulp.task('browserify-test', ['lint-test'], function() {
 		.pipe(gulp.dest('build'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('client/**/*.js', ['browserify-client']);
-	gulp.watch('test/client/**/*.js', ['browserify-test']);
+
+// Styles
+
+gulp.task('styles', function() {
+	return gulp.src('client/less/index.less')
+		.pipe(less())
+		.pipe(prefix({ cascade: true }))
+		.pipe(rename('car-finder.css'))
+		.pipe(gulp.dest('build'))
+		.pipe(gulp.dest('public/stylesheets'));
 });
+
+
+// Build
+
+gulp.task('minify', ['styles'], function() {
+	return gulp.src('build/car-finder.css')
+		.pipe(minifyCSS())
+		.pipe(rename('car-finder.min.css'))
+		.pipe(gulp.dest('public/stylesheets'));
+});
+
+gulp.task('uglify', ['browserify-client'], function() {
+	return gulp.src('build/car-finder.js')
+		.pipe(uglify())
+		.pipe(rename('car-finder.min.js'))
+		.pipe(gulp.dest('public/javascripts'));
+});
+
+
+// Test & Watch
+
+gulp.task('test', ['lint-test', 'browserify-test'], function() {
+	return gulp.src('test/client/index.html')
+		.pipe(mochaPhantomjs());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('client/**/*.js', ['browserify-client', 'test']);
+	gulp.watch('test/client/**/*.js', ['test']);
+});
+
+
+// Tasks
+
+gulp.task('build', ['uglify', 'minify']);
+gulp.task('default', ['test', 'build', 'watch']);
